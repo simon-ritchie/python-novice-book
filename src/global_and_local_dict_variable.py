b943314d01c144a76fc571c24db2d3e48afb7ca2
simon-ritchie <antisocial.sid2@gmail.com>
2020-04-19 17:14:51 +0900

    辞書の変数のスコープ関係のスクリプトを追加。

---
+++ src/global_and_local_dict_variable.py
@@ -0,0 +1,9 @@
+dict_variable = {'name': 'orange'}
+
+
+def change_dict_variable():
+    dict_variable = {'name': 'apple'}
+
+
+change_dict_variable()
+print(dict_variable)
---
+++ src/global_dict_variable.py
@@ -0,0 +1,8 @@
+dict_variable = {'name': 'orange'}
+
+
+def print_dict_variable():
+    print(dict_variable)
+
+
+print_dict_variable()
