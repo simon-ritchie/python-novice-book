64571d49d6114646067f6ef2522097b7decaadc7
simon-ritchie <antisocial.sid2@gmail.com>
2020-04-20 21:20:25 +0900

    スコープ関係の説明用のスクリプトを追加。

---
+++ src/update_global_dict_variable.py
@@ -0,0 +1,10 @@
+dict_variable = {'name': 'orange'}
+
+
+def change_dict_variable():
+    global dict_variable
+    dict_variable = {'name': 'apple'}
+
+
+change_dict_variable()
+print(dict_variable)
