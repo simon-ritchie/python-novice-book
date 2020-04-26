f5f79da12d60f07d041f9f83cbab22271f4e8fa1
simon-ritchie <antisocial.sid2@gmail.com>
2020-04-26 19:49:17 +0900

    返却値関係のスクリプトを追加。

---
+++ src/return_at_func_start.py
@@ -0,0 +1,8 @@
+def add_one(x):
+    return
+    added_value = x + 1
+    return added_value
+
+
+returned_value = add_one(x=20)
+print(returned_value)
---
+++ src/return_value.py
@@ -0,0 +1,7 @@
+def add_one(x):
+    added_value = x + 1
+    return added_value
+
+
+returned_value = add_one(x=10)
+print(returned_value)
