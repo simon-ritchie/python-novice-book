90956aebc7cf153423d3b70a23364c248e13eca1
simon-ritchie <antisocial.sid2@gmail.com>
2020-04-27 20:42:55 +0900

    複数の返却値関係の説明用のスクリプトを追加。

---
+++ src/multiple_return_value.py
@@ -0,0 +1,8 @@
+def add_one(x, y):
+    added_x = x + 1
+    added_y = y + 1
+    return added_x, added_y
+
+
+returned_x, returned_y = add_one(x=100, y=200)
+print('returned_x:', returned_x, 'returned_y:', returned_y)
