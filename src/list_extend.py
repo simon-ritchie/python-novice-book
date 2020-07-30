8af5f57291efa7e39226448599ae7fe487ad156e
simon-ritchie <antisocial.sid2@gmail.com>
2020-07-30 20:28:33 +0900

    リストのextendメソッドに対する説明用のスクリプトを追加。

---
+++ src/list_extend.py
@@ -0,0 +1,5 @@
+left_list = ['猫', '犬']
+right_list = ['狼', '兎']
+
+left_list.extend(right_list)
+print(left_list)
---
+++ src/list_extend_with_tuple_arg.py
@@ -0,0 +1,5 @@
+left_list = ['猫', '犬']
+right_tuple = ('狼', '兎')
+
+left_list.extend(right_tuple)
+print(left_list)
