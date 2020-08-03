dc9f956d827019c3cbf895c468c5d746b3f12dea
simon-ritchie <antisocial.sid2@gmail.com>
2020-08-03 20:13:06 +0900

    演習問題関係のスクリプトを追加。

---
+++ src/answer_list_extend.py
@@ -0,0 +1,4 @@
+list_left_value = ['猫', '犬']
+list_right_value = ['兎', '狼']
+list_left_value.extend(list_right_value)
+print(list_left_value)
---
+++ src/answer_list_pop.py
@@ -0,0 +1,4 @@
+list_value = ['猫', '犬', '兎']
+dog_value = list_value.pop(1)
+print('取り出した値:', dog_value)
+print('結果のリスト:', list_value)
